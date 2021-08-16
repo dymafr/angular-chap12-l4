c54f82b4a9c437330d86214d8a099075c87d7cfe
dymafr <erwan1309@hotmail.com>
2021-08-16 15:00:42 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,17 @@
-import { Component } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public form: FormGroup;
-
-  ngOnInit() {
-    this.createForm();
-  }
-
-  createForm() {
-    this.form = new FormGroup({
-      name: new FormControl(""),
-      email: new FormControl(""),
-      confirmEmail: new FormControl("")
-    });
-  }
+  public form: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    confirmEmail: new FormControl('')
+  });
 
   reinitialiser() {
     this.form.reset();
